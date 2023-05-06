12128984a658be0256894b0c4cfa9096a7d81d2b
Spenser Bushey <spenser@spenserbushey.com>
2023-05-06 17:11:33 +0000

    Fully functioning API

--- flask/api.py
+++ flask/api.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
-from db import get_covers_db
+from db import get_covers_db, get_log_db
 import datetime
+from sqlalchemy import text
+import re
 
 app = Flask(__name__)
 
@@ -10,52 +12,65 @@ def hello():
 
 @app.route('/cover/bytext')
 def get_covers_by_text():
-    search_text = request.args.get('q')
-    covers = get_covers_by_search_text(search_text)
-    save_search_log(search_text, len(covers))
+    user_given_params = request.args.get('q')
+    search_text = user_given_params
+    query = text("""
+        SELECT
+            source,
+            CONCAT('https://download.audiobookcovers.com/covers/fullres/', filename) AS filename,
+            CONCAT('https://download.audiobookcovers.com/covers/lowres/', small_image_filename) AS small_filename
+        FROM Weldawadyathink$covers.searchable_images
+        WHERE MATCH (search_text) AGAINST (:search_text)
+    """)
+    db = get_covers_db()
+    covers = db.execute(query, {"search_text": f"{search_text}"}).fetchall()
+    covers = [cover._asdict() for cover in covers]
+    save_search_log('/cover/bytext', user_given_params, search_text, len(covers))
     return jsonify(covers)
 
 @app.route('/cover/byredditpostid')
 def get_covers_by_reddit_post_id():
-    reddit_post_id = request.args.get('q')
-    covers = get_covers_by_reddit_id(reddit_post_id)
-    save_search_log(f"Reddit Post ID: {reddit_post_id}", len(covers))
+    user_given_params = request.args.get('q')
+    reddit_post_id = extract_reddit_post_id(user_given_params)
+    query = text("""
+        SELECT
+            source,
+            CONCAT('https://download.audiobookcovers.com/covers/fullres/', filename) AS filename,
+            CONCAT('https://download.audiobookcovers.com/covers/lowres/', small_image_filename) AS small_filename
+        FROM Weldawadyathink$covers.image
+        WHERE reddit_post_id = :reddit_post_id
+    """)
+    db = get_covers_db()
+    covers = db.execute(query, {"reddit_post_id": f"{reddit_post_id}"}).fetchall()
+    covers = [cover._asdict() for cover in covers]
+    save_search_log('/cover/byredditpostid', user_given_params, reddit_post_id, len(covers))
     return jsonify(covers)
 
-def save_search_log(search_text, numresults):
-    session = get_covers_db()
+def extract_reddit_post_id(input_str):
+    reddit_post_id_regex = r'^[A-Za-z0-9_-]+$'
+    
+    if re.match(reddit_post_id_regex, input_str):
+        return input_str
+
+    reddit_url_regex = r'https?://(?:www\.)?reddit\.com/r/\w+/comments/(\w+)(?:/.*)?'
+    match = re.search(reddit_url_regex, input_str)
+    
+    if match:
+        return match.group(1)
+    else:
+        raise ValueError("Invalid Reddit URL or post ID")
+
+def save_search_log(endpoint, user_given_params, query_params, num_results):
+    db = get_log_db()
     try:
-        session.execute("INSERT INTO search_log (date_time, search_text, num_results) VALUES (?, ?, ?)", (datetime.datetime.now(), search_text, numresults))
-        session.commit()
-        print(f"Search for {search_text} returned {numresults} results. Saved in database.")
+        db.execute(text("INSERT INTO search_log (date_time, endpoint, user_given_params, query_params, num_results) VALUES (:date_time, :endpoint, :user_given_params, :query_params, :num_results)"),
+                   {'date_time': str(datetime.datetime.now()), 'endpoint': str(endpoint), 'user_given_params': str(user_given_params), 'query_params': str(query_params), 'num_results': str(num_results)})
+        db.commit()
+        print(f"Search for {user_given_params} returned {num_results} results. Saved in database.")
     except Exception as e:
         print(f"Database error: {e}")
     finally:
-        session.close()
-
-def get_covers_by_search_text(search_text):
-    session = get_covers_db()
-    try:
-        results = session.execute("SELECT filename, reddit_post_id FROM Weldawadyathink$covers.image WHERE search_text LIKE ?", (f"%{search_text}%",)).fetchall()
-        covers = [{
-            "source": f"https://redd.it/{result['reddit_post_id']}",
-            "filename": f"https://r2.audiobookcovers.com/covers/{result['filename']}"
-        } for result in results]
-        return covers
-    finally:
-        session.close()
-
-def get_covers_by_reddit_id(reddit_post_id):
-    session = get_covers_db()
-    try:
-        results = session.execute("SELECT filename, reddit_post_id FROM Weldawadyathink$covers.image WHERE reddit_post_id = ?", (reddit_post_id,)).fetchall()
-        covers = [{
-            "source": f"https://redd.it/{result['reddit_post_id']}",
-            "filename": f"https://r2.audiobookcovers.com/covers/{result['filename']}"
-        } for result in results]
-        return covers
-    finally:
-        session.close()
+        db.close()
 
 if __name__ == '__main__':
     app.run()
