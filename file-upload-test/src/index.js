e9bbb63291433dc8d13372646ff11ed5bf63dad4
Spenser Bushey <spenser@spenserbushey.com>
2023-10-01 21:23:02 -0700

    google cloud vision api integration

--- file-upload-test/src/index.js
+++ file-upload-test/src/index.js
@@ -1,37 +1,60 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run "npm run dev" in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run "npm run deploy" to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
 
 export default {
   async fetch(request, env, ctx) {
-    // Parse the request to FormData
     const formData = await request.formData();
-    // Get the File from the form. Key for the file is 'image' for me
     const file = formData.get('file');
 
-    const hash = await sha1(file);
+    const annotation = await annotateImage(file, env.gcloud_api_key);
 
-    await env.bucket.put(file.name, file)
+    // await env.bucket.put(file.name, file)
 
-    return new Response(JSON.stringify({
-      name: file.name,
-      type: file.type,
-      size: file.size,
-      hash,
-    }));
+    return new Response(annotation);
   },
 };
 
-async function sha1(file) {
-  const fileData = await file.arrayBuffer();
-  const digest = await crypto.subtle.digest('SHA-1', fileData);
-  const array = Array.from(new Uint8Array(digest));
-  const sha1 =  array.map(b => b.toString(16).padStart(2, '0')).join('')
-  return sha1;
+function extractAnnotationText(annotation) {
+  const text =  annotation.responses[0].textAnnotations[0].description;
+  const removedNewlines = text.replace(/\n/g, ' ');
+  return removedNewlines;
+}
+
+async function annotateImage(imageFile, apiKey) {
+  const buffer = await imageFile.arrayBuffer();
+  const image = Buffer.from(buffer).toString('base64');
+  const endpoint = 'https://vision.googleapis.com/v1/images:annotate';
+  const requestPayload = {
+    requests: [
+      {
+        image: {
+          content: image
+        },
+        features: [
+          {
+            type: 'TEXT_DETECTION'
+          }
+        ]
+      }
+    ]
+  };
+
+  try {
+    const response = await fetch(endpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-goog-api-key': apiKey
+      },
+      body: JSON.stringify(requestPayload)
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return extractAnnotationText(data);
+
+  } catch (error) {
+    console.error('Error:', error);
+  }
 }
