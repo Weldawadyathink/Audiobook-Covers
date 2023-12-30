98c670efae51af4ac5a3e22a57d9e97a5845004f
Spenser Bushey <spenser@spenserbushey.com>
2023-12-29 21:22:11 -0800

    file upload is working

--- workers/website/src/upload.js
+++ workers/website/src/upload.js
@@ -1,57 +1,3 @@
-function uploadFile(file, signedRequest, url) {
-    const options = {
-        method: 'PUT',
-        body: file
-    };
-    return fetch(signedRequest, options)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`${response.status}: ${response.statusText}`);
-        }
-        return url;
-    });
-}
-
-
-
-
-
-
-function uploadToS3(file, source) {
-    return getSignedRequest(file, source)
-    .then(json => uploadFile(file, json.signedRequest, json.url))
-    .then(url => {
-        return url;
-    })
-    .catch(err => {
-        console.error(err);
-        return null;
-    });
-}
-
-
-
-
-
-function getSignedRequest(file, source) {
-    const api_url = "https://dev.api.audiobookcovers.com";
-    const fileName = file.name
-    const fileExtension = fileName.slice(((fileName.lastIndexOf(".") - 1) >>> 0) + 2);
-    const mime_type = file.type
-    return fetch(`${api_url}/upload/cover?source=${encodeURIComponent(source)}&extension=${encodeURIComponent(fileExtension)}&mime_type=${mime_type}`)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`${response.status}: ${response.statusText}`);
-        }
-        return response.json();
-    });
-}
-
-
-
-
-
-
 async function uploadCover() {
     const sourceUrl = document.getElementById('sourceUrl').value;
     const file = document.getElementById('fileUpload').files[0];
@@ -62,34 +8,80 @@ async function uploadCover() {
     }
 
     const fileName = file.name;
-    const fileExtension = fileName.slice(((fileName.lastIndexOf(".") - 1) >>> 0) + 2);
-    const mime_type = file.type
+    const fileExtension = fileName.split('.').pop();
+    const mime_type = file.type;
 
     if (!fileExtension) {
-        alert("Could not determine the file extension.")
-        return
+        alert("Could not determine the file extension.");
+        return;
     }
 
     if (!mime_type) {
-        alert("Could not determine the file type.")
-        return
+        alert("Could not determine the file type.");
+        return;
     }
 
     const valid_mime_types = [
         'image/png',
         'image/jpeg',
         'image/webp',
-    ]
+    ];
     if (!valid_mime_types.includes(mime_type)) {
-        alert("File format is not supported.")
+        alert("File format is not supported.");
+        return;
     }
 
-    const api_url = "https://dev.api.audiobookcovers.com";
-    const s3_signed_url = await fetch(`${api_url}/upload/cover?source=${encodeURIComponent(sourceUrl)}&extension=${encodeURIComponent(fileExtension)}&mime_type=${mime_type}`)
-    const options = {
-        method: 'PUT',
-        body: file
+    const status = await upload_to_s3(file, sourceUrl);
+    alert(status);
+}
+
+
+
+
+
+async function get_presigned_url(source, extension, mime_type) {
+    const url = `https://dev.api.audiobookcovers.com/upload/cover?extension=${extension}&source=${encodeURIComponent(source)}&mime_type=${mime_type}`;
+    const requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
     };
-    const res = await fetch(s3_signed_url.url, options)
-    console.log(res)
+    return fetch(url, requestOptions)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok: ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(result => result.url);
+}
+
+
+
+
+
+
+async function upload_to_s3(file, source) {
+
+    // Get pre-signed url
+    const mime_type = file.type;
+    const fileExtension = file.name.split('.').pop();
+    const signed_url = await get_presigned_url(source, fileExtension, mime_type);
+
+
+    // Upload to pre-signed url
+    const requestOptions = {
+        method: 'PUT',
+        body: file,
+        redirect: 'follow'
+    };
+
+    return fetch(signed_url, requestOptions)
+        .then(response => {
+            if(response.ok) {
+                console.log(response);
+                return "File uploaded successfully";
+            } else {
+                throw new Error("File upload failed");
+            }
+        });
 }
