113eabaf9a05112d593ff9c043004efe57bbb826
Spenser Bushey <spenser@spenserbushey.com>
2024-01-01 08:57:24 -0800

    proof of concept moving download controls

--- workers/website/src/search.js
+++ workers/website/src/search.js
@@ -1,52 +1,55 @@
-const searchInput = document.getElementById("search-input")
-const searchBtn = document.getElementById("search-btn")
-const allResultsContainer = document.getElementById("results-container")
-const searchTypeInputs = document.querySelectorAll(".search-type-input")
+const searchInput = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-btn");
+const allResultsContainer = document.getElementById("results-container");
+const searchTypeInputs = document.querySelectorAll(".search-type-input");
 
-searchBtn.addEventListener("click", search)
+searchBtn.addEventListener("click", search);
 searchInput.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
-    event.preventDefault()
-    search()
+    event.preventDefault();
+    search();
   }
-})
+});
 
 async function search() {
-  let query = searchInput.value
+  let query = searchInput.value;
   const selectedSearchType = Array.from(searchTypeInputs).find(
     (input) => input.checked
-  ).value
+  ).value;
 
-  let apiUrl = ""
+  let apiUrl = "";
   if (selectedSearchType === "text") {
-    apiUrl = `https://dev.api.audiobookcovers.com/cover/bytext/?q=${encodeURIComponent(query)}`
+    apiUrl = `https://dev.api.audiobookcovers.com/cover/bytext/?q=${encodeURIComponent(
+      query
+    )}`;
   } else {
-    apiUrl = `https://dev.api.audiobookcovers.com/cover/byredditpostid/?q=${encodeURIComponent(query)}`
+    apiUrl = `https://dev.api.audiobookcovers.com/cover/byredditpostid/?q=${encodeURIComponent(
+      query
+    )}`;
   }
 
   try {
-    const response = await fetch(apiUrl)
+    const response = await fetch(apiUrl);
     if (!response.ok) {
-      throw new Error(response.status)
+      throw new Error(response.status);
     }
-    const results = await response.json()
-    displayResults(results)
+    const results = await response.json();
+    displayResults(results);
   } catch (error) {
-    displayError(error)
+    displayError(error);
   }
 
-  searchInput.blur() // Remove the focus from the input element
+  searchInput.blur(); // Remove the focus from the input element
 }
 
-
 function displayResults(results) {
-  allResultsContainer.innerHTML = ""
+  allResultsContainer.innerHTML = "";
 
   if (results.length === 0) {
-    const noResultsMessage = document.createElement("p")
-    noResultsMessage.textContent = "No results found."
-    allResultsContainer.appendChild(noResultsMessage)
-    return
+    const noResultsMessage = document.createElement("p");
+    noResultsMessage.textContent = "No results found.";
+    allResultsContainer.appendChild(noResultsMessage);
+    return;
   }
 
   results.forEach((result) => {
@@ -68,11 +71,6 @@ function displayResults(results) {
     coverImage.addEventListener("click", (event) => flipCard(event.target));
     front.appendChild(coverImage);
 
-
-
-
-
-
     // Source Link
     const sourceLink = document.createElement("a");
     sourceLink.href = result.source;
@@ -87,50 +85,28 @@ function displayResults(results) {
 
     front.appendChild(sourceLink);
 
-
-
-
-
     // Share Link
     const shareLink = document.createElement("img");
     shareLink.className = "corner_bottom_left";
-    shareLink.addEventListener("click", () => copyToClipboard("https://google.com"));
+    shareLink.addEventListener("click", () =>
+      copyToClipboard("https://google.com")
+    );
     shareLink.src = "share.svg";
     shareLink.alt = "Get sharing link";
 
     front.appendChild(shareLink);
 
-
-
-
     // Back of card
     const back = document.createElement("div");
     back.className = "back fill_cover rounded";
-
-    // Back button
-    const backButton = document.createElement("img");
-    backButton.className = "corner_top_left"
-    backButton.addEventListener("click", (event) => flipCard(event.target))
-    backButton.src = "navigate_before.svg";
-    backButton.alt = "Go back";
-    back.appendChild(backButton);
-
-
-    
-
 
     // Apply to results container
     card.appendChild(front);
     card.appendChild(back);
     resultContainer.appendChild(card);
     allResultsContainer.appendChild(resultContainer);
-  })
+  });
 }
-
-
-
-
-
 
 function downloadFile(url) {
   const link = document.createElement("a");
@@ -141,27 +117,27 @@ function downloadFile(url) {
   document.body.removeChild(link);
 }
 
-
-
-
-
-
 function flipCard(target) {
-    const classToFlip = "can_be_flipped";
-    let element = target;
-    while (element && element !== document.body && !element.classList.contains(classToFlip)) {
-        element = element.parentElement;
-    }
-    const elementsToFlip = new Set([element, ...document.querySelectorAll(".can_be_flipped.flipped")]);
-    elementsToFlip.forEach((element) => {
-        element.classList.toggle("flipped");
-    });
+  const classToFlip = "can_be_flipped";
+  let element = target;
+  while (
+    element &&
+    element !== document.body &&
+    !element.classList.contains(classToFlip)
+  ) {
+    element = element.parentElement;
+  }
+  const download_form = document.querySelector("div.download_selection_window");
+  const card_back = element.querySelector("div.back");
+  card_back.appendChild(download_form);
+  const elementsToFlip = new Set([
+    element,
+    ...document.querySelectorAll(".can_be_flipped.flipped"),
+  ]);
+  elementsToFlip.forEach((element) => {
+    element.classList.toggle("flipped");
+  });
 }
-
-
-
-
-
 
 function displayError(error) {
   allResultsContainer.innerHTML = "";
@@ -170,17 +146,20 @@ function displayError(error) {
   allResultsContainer.appendChild(errorMessage);
 }
 
-
-
-
-
-
-
 function copyToClipboard(text) {
-  navigator.clipboard.writeText(text).then(() => {
-    alert('Link copied to clipboard!');
-  }).catch(err => {
-    console.error('Failed to copy: ', err);
-  });
+  navigator.clipboard
+    .writeText(text)
+    .then(() => {
+      alert("Link copied to clipboard!");
+    })
+    .catch((err) => {
+      console.error("Failed to copy: ", err);
+    });
 }
 
+window.addEventListener("storage", (event) => {
+  if (event.key === "download_type") {
+  }
+  if (event.key === "download_size") {
+  }
+});
