aeac33cb7b2470e84a5c13678bf660dc4173b95f
Spenser Bushey <spenser@spenserbushey.com>
2024-01-08 12:07:46 -0800

    format for scheduled events

--- workers/updater/src/index.js
+++ workers/updater/src/index.js
@@ -11,13 +11,11 @@
 import { extract } from '@extractus/feed-extractor';
 
 export default {
-	async fetch(request, env, ctx) {
-		const headers = {
-			'Content-Type': 'application/json',
-			'Access-Control-Allow-Origin': '*',
-			'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-		};
-		const result = await extract('https://www.reddit.com/r/AudiobookCovers/.rss');
-		return new Response(JSON.stringify(result), { headers: headers });
+	async scheduled(request, env, ctx) {
+		const feed = await extract('https://www.reddit.com/r/AudiobookCovers/new.rss');
+		const reddit_ids = feed.entries.map((item) => {
+			return item.id.split('_')[1];
+		});
+		console.log(JSON.stringify(reddit_ids));
 	},
 };
