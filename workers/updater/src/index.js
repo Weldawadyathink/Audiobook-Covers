d53dea12200777062b7cf85f0e76f36a6bf4678e
Spenser Bushey <spenser@spenserbushey.com>
2024-01-09 19:38:09 -0800

    scheduled export to fauna db

--- workers/updater/src/index.js
+++ workers/updater/src/index.js
@@ -1,14 +1,6 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 import { extract } from '@extractus/feed-extractor';
+
+import { Client, fql, ServiceError } from 'fauna';
 
 export default {
 	async scheduled(request, env, ctx) {
@@ -16,6 +8,60 @@ export default {
 		const reddit_ids = feed.entries.map((item) => {
 			return item.id.split('_')[1];
 		});
-		console.log(JSON.stringify(reddit_ids));
+		// const index = get_algolia_index(env);
+		// reddit_ids.filter(check_id_in_algolia).map((id) => insert_into_algolia(index, id));
+		const fauna = new Client({
+			secret: env.FAUNA_SECRET,
+		});
+
+		reddit_ids.map((id) => insert_into_fauna(fauna, id));
 	},
 };
+
+async function insert_into_fauna(fauna, post_id) {
+	const query = fql`
+		RedditPost.create({
+			"postId": ${post_id},
+			"status": "new",
+		})
+	`;
+	await fauna.query(query);
+}
+
+async function insert_into_algolia(index, id) {
+	index.saveObject({
+		objectID: id,
+		downloaded: false,
+	});
+}
+
+function get_algolia_index(env) {
+	const client = algoliasearch(env.ALGOLIA_APP_ID, env.ALGOLIA_SEARCH_KEY, {
+		requester: createFetchRequester(),
+	});
+	return client.initIndex('redditPosts');
+}
+
+async function check_id_in_algolia(index, id) {
+	/**
+	 * Checks if a given UUID exists in the Algolia index.
+	 *
+	 * @param {object} index - The Algolia index object.
+	 * @param {string} id - The UUID to check.
+	 * @returns {boolean} True if the UUID is valid and usable, false if it is a duplicate.
+	 */
+	try {
+		const response = await index.getObject(id, {
+			attributesToRetrieve: ['objectID'],
+		});
+	} catch (error) {
+		if (!error.message.includes('ObjectID does not exist')) {
+			throw error;
+		} else {
+			// If it errors, id is not in index and therefore is valid
+			return true;
+		}
+	}
+	// If error was not thrown, the id exists in the index, and therefore is not valid
+	return false;
+}
