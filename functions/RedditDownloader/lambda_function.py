64a19084498ef637fc465d82597fd3f5ae7925dd
Spenser Bushey <spenser@spenserbushey.com>
2024-01-09 21:28:07 -0800

    Can't get python to call bdfr

--- functions/RedditDownloader/lambda_function.py
+++ functions/RedditDownloader/lambda_function.py
@@ -1,7 +1,22 @@
-from fauna import fql
-from fauna.client import Client
-from fauna.encoding import QuerySuccess
-from fauna.errors import FaunaException
+import subprocess
+import os
+import sys
 
 def lambda_handler(event, context):
+    include_id = "192mt7x"
+    include_file = "/tmp/include-id.txt"
+    with open(include_file, "w+") as f:
+        f.write(include_id)
+        
+    add_to_path_command = "export PATH=$PATH:/opt/python/lib/python3.11/site-packages"
+    # print(f"Python executable is {sys.executable}")
+    # print(f"System path is {sys.path}")
+    # print(os.listdir('/opt/python/lib/python3.11/site-packages'))
+    # print(os.listdir('/opt/python/lib/python3.11/site-packages/bdfr'))
+    # subprocess.run([sys.executable,'-c','import sys; print(sys.path)'])
+    sys.path.append(add_to_path_command)
+    subprocess.run(f"{sys.executable} -m bdfr download /tmp --include-id-file {include_file} --file-scheme {{POSTID}}".split(" "))
+    print(os.listdir('/tmp'))
+
+if __name__ == '__main__':
     pass
