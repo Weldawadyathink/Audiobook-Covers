ec1d6f16c92f1949c631cf1a8bc673b97fa4c6bc
Spenser Bushey <spenser@spenserbushey.com>
2024-01-12 20:07:10 -0800

    working processing from fauna queue

--- functions/RedditDownloader/faunadb.py
+++ functions/RedditDownloader/faunadb.py
@@ -8,8 +8,11 @@ from botocore.exceptions import ClientError
 
 import json
 
-def get_algolia_client():
-    secret_name = "audiobookcovers/algolia/key/write"
+class NoMoreItems(Exception):
+    pass
+
+def get_fauna_client():
+    secret_name = "audiobookcovers/fauna/key/write"
     region_name = "us-west-1"
 
     session = boto3.session.Session()
@@ -27,5 +30,28 @@ def get_algolia_client():
         # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
         raise e
 
-    secret = json.loads(get_secret_value_response['SecretString'])
-    return secret['Application_ID'], secret['API_Key']
+    secret = get_secret_value_response['SecretString']
+    return Client(secret=secret)
+
+def get_post_id_to_download():
+    try:
+        client = get_fauna_client()
+        query = fql('RedditPost.where(.status == "new").take(1)')
+        response = client.query(query)
+        post_id = response.data.data[0]["postId"]
+    except IndexError:
+        raise NoMoreItems()
+
+def mark_post_id_complete(post_id):
+    client = get_fauna_client()
+    query = fql(
+        "RedditPost.where(.postId == ${post_id}).first().updateData({status: 'downloaded'})",
+        post_id=post_id
+    )
+    print(query)
+    response = client.query(query)
+    return response
+
+if __name__ == "__main__":
+    print(get_post_id_to_download())
+    print(mark_post_id_complete("1902fqd"))
--- functions/RedditDownloader/lambda_function.py
+++ functions/RedditDownloader/lambda_function.py
@@ -2,6 +2,10 @@ import subprocess
 import os
 import sys
 import shutil
+import boto3
+from uuid import uuid1
+from algolia import generate_unique_uuid
+from faunadb import get_post_id_to_download, mark_post_id_complete, NoMoreItems
 
 include_file = "/tmp/include-id.txt"
 download_folder = "/tmp/downloads"
@@ -12,13 +16,19 @@ config_file = "/tmp/config.cfg"
 
 def lambda_handler(event, context):
     prepare_environment()
+    try:
+        include_id = get_post_id_to_download()
+    except NoMoreItems:
+        # Recursive lambda function exit point
+        return
     
-    include_id = "192mt7x"
     with open(include_file, "w+") as f:
         f.write(include_id)
     subprocess.run(f"{sys.executable} -m bdfr download /tmp/downloads --config {config_file} --include-id-file {include_file} --file-scheme {{POSTID}}".split(" "))
-    print(os.listdir("/tmp/downloads/"))
-    print([file for file in os.walk("/tmp/downloads/")])
+    print(f"Downloaded files for post {include_id} with BDFR")
+    process_all_files()
+    mark_post_id_complete(include_id)
+    
 
 def prepare_environment():
     shutil.copy(original_config_file, config_file)
@@ -47,6 +57,45 @@ def prepare_environment():
         print(f"All contents of {download_folder} have been deleted.")
     else:
         os.makedirs(download_folder)
+
+
+def process_file(directory_name, file_name):
+    complete_file_path = os.path.join(directory_name, file_name)
+    image_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.webp']
+    image_bucket = "com-audiobookcovers-uploads"
+    other_bucket = "com-audiobookcovers-other"
+    
+    s3 = boto3.client("s3")
+    
+    reddit_post_id = file_name[:7]
+    print(f"Processing {file_name}, post id {reddit_post_id}")
+    
+    if os.path.splitext(file_name)[1].lower() in image_extensions:
+        # Process image file
+        new_file_key = "|".join([
+            os.path.splitext(file_name)[1].lower().replace(".", ""),
+            generate_unique_uuid(),
+            f"https://reddit.com/{reddit_post_id}",
+        ])
+        s3.upload_file(complete_file_path, image_bucket, new_file_key)
+        print(f"Uploaded {complete_file_path} to {image_bucket}/{new_file_key}")
+    else:
+        new_file_key = "|".join([
+            str(uuid1()),
+            file_name,
+        ])
+        s3.upload_file(complete_file_path, other_bucket, new_file_key)
+        print(f"Uploaded {complete_file_path} to {other_bucket}/{new_file_key}")
+    
+    return reddit_post_id
+
+    
+
+
+def process_all_files():
+    for dirpath, dirnames, filenames in os.walk(download_folder):
+        for filename in filenames:
+            process_file(dirpath, filename)
     
 
 if __name__ == '__main__':
