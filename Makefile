c0b2d66b5e2e92ba0098d2b99661b88bab67e0ba
Spenser Bushey <spenser@spenserbushey.com>
2024-01-15 09:04:35 -0800

    Potentially working cloud run api

--- Makefile
+++ Makefile
@@ -6,7 +6,9 @@ ALL_LAYERS_MAKEFILES := $(shell find ./layers -name Makefile)
 
 ALL_WORKERS_MAKEFILES := $(shell find ./workers -name Makefile)
 
-deploy: deploy_lambda_layers deploy_lambda_functions deploy_workers
+ALL_CLOUD_RUN_MAKEFILES := $(shell find ./cloud-run -name Makefile)
+
+deploy: deploy_lambda_layers deploy_lambda_functions deploy_workers deploy_cloud_run
 
 deploy_lambda_functions:
 	@echo "Deploying all lambda functions"
@@ -25,6 +27,13 @@ deploy_lambda_layers:
 deploy_workers:
 	@echo "Deploying all Cloudflare Workers"
 	@for makefile in $(ALL_WORKERS_MAKEFILES); do \
+		echo Deploying $$makefile; \
+		$(MAKE) -C $$(dirname $$makefile) deploy; \
+	done
+
+deploy_cloud_run:
+	@echo "Deploying all Cloud Run images"
+	@for makefile in $(ALL_CLOUD_RUN_MAKEFILES); do \
 		echo Deploying $$makefile; \
 		$(MAKE) -C $$(dirname $$makefile) deploy; \
 	done
--- cloud-run/clip-ai-lookup/main.py
+++ cloud-run/clip-ai-lookup/main.py
@@ -1,17 +1,40 @@
 import os
 from sentence_transformers import SentenceTransformer
 from flask import Flask, request
+import json
+import pinecone
 
 app = Flask(__name__)
+
+def get_model():
+    if os.environ.get("GOOGLE_CLOUD_RUN") is None:
+        return SentenceTransformer('clip-ViT-B-32')
+    # Use local model file if in google cloud run
+    SentenceTransformer('./clip-ViT-B-32')
+
+model = get_model()
+
+def pinecone_init():
+    secret_name = "PINECONE"
+    secret = json.loads(os.environ.get(secret_name))
+    pinecone.init(
+        api_key = secret["api_key"],
+        environment = secret["environment"],
+    )
 
 
 @app.route("/text")
 def text_search():
     query = request.args.get("q")
-    print("Loading Model")
-    model = SentenceTransformer('./clip-ViT-B-32')
-    print("Running model")
-    return model.encode("Hello world!")
+    pinecone_init()
+    index = pinecone.index("covers")
+    vector = model.encode(query).tolist()
+    return index.query(
+        vector = vector,
+        include_metadata = True,
+        include_values = False,
+        top_k = 50,
+    )
 
 def get_model():
     if os.environ.get("GOOGLE_CLOUD_RUN") is None:
