b3181a3a8c7c130e35378ee74d96fa028725c67b
Spenser Bushey <spenser@spenserbushey.com>
2024-01-16 15:44:30 -0800

    Working in gcloud

--- cloud-run/clip-ai-lookup/main.py
+++ cloud-run/clip-ai-lookup/main.py
@@ -2,7 +2,7 @@ import os
 from sentence_transformers import SentenceTransformer
 from flask import Flask, request
 import json
-import pinecone
+from pinecone import Pinecone
 
 app = Flask(__name__)
 
@@ -10,38 +10,30 @@ def get_model():
     if os.environ.get("GOOGLE_CLOUD_RUN") is None:
         return SentenceTransformer('clip-ViT-B-32')
     # Use local model file if in google cloud run
-    SentenceTransformer('./clip-ViT-B-32')
+    return SentenceTransformer('./clip-ViT-B-32')
 
 model = get_model()
 
 def pinecone_init():
-    secret_name = "PINECONE"
-    secret = json.loads(os.environ.get(secret_name))
-    pinecone.init(
+    secret = json.loads(os.environ.get("PINECONE"))
+    return Pinecone(
         api_key = secret["api_key"],
-        environment = secret["environment"],
     )
 
 
 @app.route("/text")
 def text_search():
     query = request.args.get("q")
-    pinecone_init()
-    index = pinecone.index("covers")
+    pc = pinecone_init()
+    index = pc.Index("covers")
     vector = model.encode(query).tolist()
-    return index.query(
+    response = index.query(
         vector = vector,
         include_metadata = True,
         include_values = False,
         top_k = 50,
     )
-
-def get_model():
-    if os.environ.get("GOOGLE_CLOUD_RUN") is None:
-        return SentenceTransformer('clip-ViT-B-32')
-    # Use local model file if in google cloud run
-    SentenceTransformer('./clip-ViT-B-32')
-
+    return json.dumps(response.to_dict())
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
