5a67d389460009d4ecfce412aec975e6d30b4af8
Spenser Bushey <spenser@spenserbushey.com>
2024-01-24 15:43:08 -0800

    deploy neon vector indexer

--- cloud-run/vector-index/main.py
+++ cloud-run/vector-index/main.py
@@ -1,10 +1,9 @@
 import os
 from sentence_transformers import SentenceTransformer
-import json
-from pinecone import Pinecone
-from algoliasearch.search_client import SearchClient
 import requests
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
+import psycopg2
+from psycopg2 import sql
 
 def get_model():
     if os.environ.get("GOOGLE_CLOUD_RUN") is None:
@@ -12,58 +11,38 @@ def get_model():
     # Use local model file if in google cloud run
     return SentenceTransformer('./clip-ViT-B-32')
 
-model = get_model()
-
-def pinecone_init():
-    secret = json.loads(os.environ.get("PINECONE"))
-    return Pinecone(
-        api_key = secret["api_key"],
-    )
-
-
-def get_algolia_client():
-    secret = json.loads(os.environ.get("ALGOLIA"))
-    return SearchClient.create(secret['Application_ID'], secret['API_Key'])
+def index_one(model, db):
+    cursor = db.cursor()
+    cursor.execute(sql.SQL('SELECT id FROM image WHERE embedding IS NULL AND index_error IS NOT TRUE ORDER BY RANDOM() LIMIT 1'))
+    response = cursor.fetchone()
+    if not response:
+        print("No more images to index")
+        return False
+    id = response[0]
+    file_url = f"https://download.audiobookcovers.com/png/1000/{id}.png"
+    response = requests.get(file_url, stream=True)
+    image = ""
+    try:
+        image = Image.open(response.raw)
+    except UnidentifiedImageError as e:
+        print(f"Error opening image {id}. Flagging in database. ")
+        cursor.execute(sql.SQL("UPDATE image SET index_error = TRUE WHERE id = %s"), [id])
+        db.commit()
+        return True
+    vector = model.encode(image).tolist()
+    cursor.execute(sql.SQL("UPDATE image SET embedding = %s WHERE id = %s"), (vector, id))
+    db.commit()
+    print(f"Added vector for {id}")
+    return True
+    
     
 
 def main():
     model = get_model()
-    
-    algolia = get_algolia_client()
-    algolia_index = algolia.init_index('bookCoverIndex')
-    
-    pc = pinecone_init()
-    pc_index = pc.Index("covers")
-    num_added = 0
-    
-    for item in algolia_index.browse_objects():
-        try:
-            index_status = item['pinecone']
-        except KeyError:
-            index_status = 'not complete'
-        if not index_status == 'complete':
-            file_url = f"https://download.audiobookcovers.com/png/1000/{item['objectID']}.png"
-            response = requests.get(file_url, stream=True)
-            if not response.ok:
-                print(f"Failed to download {file_url}")
-                continue
-            image = Image.open(requests.get(file_url, stream=True).raw)
-            vectors = model.encode(image).tolist()
-            pc_index.upsert([(
-                item['objectID'],
-                vectors,
-                {
-                    'source': item['source'],
-                    'extension': item['extension'],
-                }
-            )])
-            algolia_index.partial_update_object({
-                'objectID': item['objectID'],
-                'pinecone': 'complete',
-            })
-            num_added += 1
-            print(f"Added {item['objectID']} to pinecone index")
-    print(f"Index complete, added {num_added} items to pinecone")
+    db = psycopg2.connect(os.environ.get("DATABASE"))
+    while index_one(model, db):
+        pass
+    print("Index complete")
 
 
 if __name__ == "__main__":
