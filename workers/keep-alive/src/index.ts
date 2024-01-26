23a1389feda0ad1cac948cf6e4db329c11963407
Spenser Bushey <spenser@spenserbushey.com>
2024-01-25 20:36:45 -0800

    partially working keep alive

--- workers/keep-alive/src/index.ts
+++ workers/keep-alive/src/index.ts
@@ -12,6 +12,8 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { neon } from '@neondatabase/serverless';
+
 export interface Env {
 	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
 	// MY_KV_NAMESPACE: KVNamespace;
@@ -36,15 +38,11 @@ export default {
 	// The scheduled handler is invoked at the interval set in our wrangler.toml's
 	// [[triggers]] configuration.
 	async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext): Promise<void> {
-		// A Cron Trigger can make requests to other endpoints on the Internet,
-		// publish to a Queue, query a D1 Database, and much more.
-		//
-		// We'll keep it simple and make an API call to a Cloudflare API:
-		let resp = await fetch('https://api.cloudflare.com/client/v4/ips');
-		let wasSuccessful = resp.ok ? 'success' : 'fail';
-
-		// You could store this result in KV, write to a D1 Database, or publish to a Queue.
-		// In this template, we'll just log the result:
-		console.log(`trigger fired at ${event.cron}: ${wasSuccessful}`);
+		const sql = neon(env.DATABASE);
+		const last_api_call = await sql`select max(timestamp) from api_log;`;
+		const last_api_call_time = new Date(last_api_call[0]);
+		const minutes = (Date() - last_api_call_time) / 60000;
+		console.log(last_api_call)
+		console.log(`trigger fired at`);
 	},
 };
