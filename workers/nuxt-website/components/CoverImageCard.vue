4ac9c2bcd438a46dea72b220ece833944dc6d695
Spenser Bushey <spenser@spenserbushey.com>
2024-02-01 10:10:12 -0800

    working backs of cards with links

--- workers/nuxt-website/components/CoverImageCard.vue
+++ workers/nuxt-website/components/CoverImageCard.vue
@@ -1,11 +1,19 @@
 <template>
     <div class="hover:scale-110 ease-in-out duration-300 w-80 h-80 aspect-square">
         <div :class="{ flipped: isFlipped }" class="card w-full h-full">
-            <div @click="flipCard" class="front rounded-3xl overflow-hidden"> 
+            <div @click="flipCard" class="front"> 
                 <img :src="imageData.versions.webp['200']" class="w-full h-full object-contain" />
             </div>
-            <div @click="flipCard" class="back bg-gray-900 border-red-800 rounded-3xl overflow-hidden">
-                
+            <div @click="flipCard" class="back bg-gray-900 border-red-800">
+                <a :href="`/image/${imageData.id}`" @click.stop class="option-pill">
+                    Link to this image
+                </a>
+                <a :href="imageData.source" @click.stop target="_blank" class="option-pill">
+                    Image Source
+                </a>
+                <a :href="`/similar/${imageData.id}`" @click.stop class="option-pill">
+                    Find similar images
+                </a>
             </div>
         </div>
     </div>
@@ -16,7 +24,7 @@ export default {
     props: ['imageData'],
     data() {
         return {
-            isFlipped: false, // Initial state of isFlipped
+            isFlipped: false,
         };
     },
     methods: {
@@ -35,6 +43,7 @@ export default {
 }
 
 .front, .back {
+    @apply rounded-3xl overflow-hidden;
     backface-visibility: hidden;
     position: absolute;
     width: 100%;
@@ -49,6 +58,10 @@ export default {
     transition: transform 0.6s;
     transform-style: preserve-3d;
     cursor: pointer;
+}
+
+.option-pill {
+    @apply block m-4 p-2 rounded-full bg-emerald-400 text-center gap-6;
 }
 
 </style>
---
+++ workers/nuxt-website/pages/similar/[id].vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Route Information for /similar/{id}</h1>
+        <p><strong>ID:</strong> {{ id }}</p>
+    </div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const id = route.params.id; // Access the dynamic segment 'id'
+</script>
