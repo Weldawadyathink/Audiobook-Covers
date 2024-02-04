0339319f2683d8eed38f9601f7f5dc33bd9e69d2
Spenser Bushey <spenser@spenserbushey.com>
2024-02-03 19:40:19 -0800

    normalize vectors for clip service

--- containers/openai-clip-server/main.py
+++ containers/openai-clip-server/main.py
@@ -2,6 +2,7 @@ import os
 from sentence_transformers import SentenceTransformer
 from flask import Flask, request, Response
 import json
+import numpy as np
 
 app = Flask(__name__)
 
@@ -10,8 +11,9 @@ model = SentenceTransformer('./clip-ViT-B-32')
 @app.route("/embedding/text")
 def get_text_embedding():
     query = request.args.get('q')
-    embedding = model.encode(query).tolist()
-    return Response(json.dumps(embedding), content_type='application/json')
+    vector = model.encode(query).tolist()
+    unit_vector = vector / np.linalg.norm(vector)
+    return Response(json.dumps(unit_vector.tolist()), content_type='application/json')
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
