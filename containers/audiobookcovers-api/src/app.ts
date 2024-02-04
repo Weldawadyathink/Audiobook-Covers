bf9c0fca4284ba1295a52fb0fb64ab1f9c19766f
Spenser Bushey <spenser@spenserbushey.com>
2024-02-03 18:36:21 -0800

    Database logging added

--- containers/audiobookcovers-api/src/app.ts
+++ containers/audiobookcovers-api/src/app.ts
@@ -5,7 +5,7 @@ import express, {
   NextFunction,
   Router,
 } from "express";
-import pool from "./db";
+import { query } from "./db";
 import { generateResponseObject, Item, ResponseObject } from "./utils";
 
 const app: Application = express();
@@ -27,6 +27,25 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   next();
 });
 
+app.use(async (req: Request, res: Response, next: NextFunction) => {
+  const start = performance.now();
+  res.on("finish", async () => {
+    const duration = Math.trunc(performance.now() - start);
+    const method = req.method;
+    const url = req.originalUrl;
+    const path = req.path;
+    const status = res.statusCode;
+    const user_agent = req.headers["user-agent"];
+    const origin = req.headers["origin"];
+    await query(
+      `INSERT INTO api_log (url, endpoint, user_agent, origin, request_time, method, status_code)
+      VALUES ($1, $2, $3, $4, $5, $6, $7)`,
+      [url, path, user_agent, origin, duration, method, status]
+    );
+  });
+  next();
+});
+
 const router: Router = express.Router({ strict: false });
 
 router.get("/", (req: Request, res: Response) => {
@@ -39,37 +58,25 @@ router.get("/cover/bytext", (req: Request, res: Response) => {
   }
   const searchString: string = <string>req.query.q;
   const formattedSearchString = searchString.split(" ").join(" & ");
-  pool.query(
-    `
-    SELECT
+  query(
+      `SELECT
       id,
       extension,
       source
     FROM image
     WHERE
       to_tsvector('english', cloud_vision_text) @@
-      to_tsquery('english', $1);
-  `,
-    [formattedSearchString]
-  ).then((result: any) => result.rows)
-  .then((items: any) => items.map(generateResponseObject))
-  .then((response_obj: [ResponseObject]) => res.status(200).send(response_obj))
-
-  // return res.status(200).send(formattedSearchString);
+      to_tsquery('english', $1);`,
+      [formattedSearchString]
+    )
+    .then((result: any) => result.rows)
+    .then((items: any) => items.map(generateResponseObject))
+    .then((response_obj: [ResponseObject]) =>
+      res.status(200).send(response_obj)
+    );
 });
 
 app.use(router);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
-
-process.on("SIGINT", async () => {
-  console.log("SIGINT signal received: closing HTTP server");
-  try {
-    await pool.end();
-    console.log("Database pool has been closed");
-  } catch (error) {
-    console.error("Error closing the database pool", error);
-  }
-  process.exit(0);
-});
--- containers/audiobookcovers-api/src/db.ts
+++ containers/audiobookcovers-api/src/db.ts
@@ -4,4 +4,24 @@ const pool: Pool = new Pool({
   connectionString: process.env.DATABASE,
 });
 
-export default pool;
+export const query = async (text: string, params: Array<any>) => {
+  const start = performance.now();
+  const res = await pool.query(text, params);
+  const duration = Math.trunc(performance.now() - start);
+  console.log(`Executed query: { rows: ${res.rowCount}, duration: ${duration}} `);
+  return res;
+};
+
+export const getClient = () => {
+  return pool.connect();
+};
+
+process.on("SIGINT", async () => {
+  console.log("SIGINT signal received: closing HTTP server");
+  try {
+    await pool.end();
+    console.log("Database pool has been closed");
+  } catch (error) {
+    console.error("Error closing the database pool", error);
+  }
+});
