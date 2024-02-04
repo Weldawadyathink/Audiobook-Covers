cf8bd21a2049073919188ffb10f1e9d816ed8da3
Spenser Bushey <spenser@spenserbushey.com>
2024-02-04 00:05:00 -0800

    fixed stupid issues with when the model was loaded

--- containers/openai-clip-server/main.py
+++ containers/openai-clip-server/main.py
@@ -6,11 +6,14 @@ import numpy as np
 
 app = Flask(__name__)
 
-model = SentenceTransformer('./clip-ViT-B-32')
+@app.route("/")
+def status():
+    return Response("Working!")
 
 @app.route("/embedding/text")
 def get_text_embedding():
     query = request.args.get('q')
+    model = SentenceTransformer('./clip-ViT-B-32')
     vector = model.encode(query).tolist()
     unit_vector = vector / np.linalg.norm(vector)
     return Response(json.dumps(unit_vector.tolist()), content_type='application/json')
