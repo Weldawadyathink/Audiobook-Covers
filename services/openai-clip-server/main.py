d992e9a4479b5e6c8a727236ef70c65fbc6460e8
Spenser Bushey <spenser@spenserbushey.com>
2024-02-04 20:23:34 -0800

    added image url endpoint to clip server

--- services/openai-clip-server/main.py
+++ services/openai-clip-server/main.py
@@ -4,6 +4,8 @@ from flask import Flask, request, Response
 import json
 import numpy as np
 from functools import cache
+import requests
+from PIL import Image, UnidentifiedImageError
 
 app = Flask(__name__)
 
@@ -20,6 +22,21 @@ def get_text_embedding():
     query = request.args.get('q')
     model = get_model()
     vector = model.encode(query).tolist()
+    unit_vector = vector / np.linalg.norm(vector)
+    return Response(json.dumps(unit_vector.tolist()), content_type='application/json')
+
+@app.route("/embedding/image/url")
+def get_image_embedding():
+    url = request.args.get('url')
+    response = requests.get(url, stream=True)
+    image = ''
+    try:
+        image = Image.open(response.raw)
+    except UnidentifiedImageError as e:
+        print(f"Error opening image {id}. Flagging in database. ")
+        return Response("Could not open image url", status=422)
+    model = get_model()
+    vector = model.encode(image)
     unit_vector = vector / np.linalg.norm(vector)
     return Response(json.dumps(unit_vector.tolist()), content_type='application/json')
 
