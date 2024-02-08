62c5cd09b2fce1039284a4ec6f3e207892cb3735
Spenser Bushey <spenser@spenserbushey.com>
2024-02-08 10:30:38 -0800

    fix api page text color

--- services/website/pages/api.vue
+++ services/website/pages/api.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="m-6">
-    <h1 class="text-l">API Reference</h1>
-    <p>
+  <div class="m-12 text-white">
+    <h1 class="text-xl my-2">API Reference</h1>
+    <p class="my-2">
       The API is free to use in your projects. If you do use it,
       please let me know about the project. 
     </p>
-    <a href="/openapi.yaml">OpenAPI Specification</a>
+    <a href="/openapi.yaml" class="text-blue-500 underline hover:text-blue-800 my-4">OpenAPI Specification</a>
   </div>
 </template>
 
