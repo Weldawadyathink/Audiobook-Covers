2d62e01c63114edb42158c0045ad46d6c5daab72
Spenser Bushey <spenser@spenserbushey.com>
2024-08-05 10:49:56 -0700

    Added download and source button

--- src/app/image/[imageId]/page.tsx
+++ src/app/image/[imageId]/page.tsx
@@ -8,10 +8,13 @@ import Tilt from "react-parallax-tilt";
 import { getBlurhashUrl } from "@/lib/blurhash";
 import { Button } from "@/components/ui/button";
 import { Spinner } from "@/components/Spinner";
+import Link from "next/link";
+import useDownloader from "react-use-downloader";
 
 const maxSimilarityLevel = 5;
 
 export default function Page({ params }: { params: { imageId: string } }) {
+  const { download } = useDownloader();
   const image = api.cover.getCover.useQuery(params.imageId);
   const [similarity, setSimilarity] = useState(1);
   const similar = api.cover.getSimilar.useQuery({
@@ -32,24 +35,37 @@ export default function Page({ params }: { params: { imageId: string } }) {
 
   return (
     <>
-      <h1 className="mx-6">{params.imageId}</h1>
       {image.status === "pending" && <Spinner />}
       {image.status === "error" && <span>Could not find image</span>}
-      <Tilt tiltMaxAngleX={maxAngle} tiltMaxAngleY={maxAngle}>
-        <div className="relative m-8 aspect-square overflow-hidden rounded-3xl">
-          {image.status === "success" && (
-            <Image
-              src={image.data.url}
-              alt="Audiobook cover image"
-              fill={true}
-              placeholder="blur"
-              priority={true}
-              // TODO: See if this function runs a lot and memoize
-              blurDataURL={getBlurhashUrl(image.data.blurhash)}
-            />
-          )}
-        </div>
-      </Tilt>
+      {image.status === "success" && (
+        <>
+          <div className="flex flex-row justify-center gap-6">
+            <Button
+              onClick={() =>
+                download(image.data.url, image.data.url.split("/").pop()!)
+              }
+            >
+              Download
+            </Button>
+            <Button asChild>
+              <Link href={image.data.source}>Source</Link>
+            </Button>
+          </div>
+          <Tilt tiltMaxAngleX={maxAngle} tiltMaxAngleY={maxAngle}>
+            <div className="relative m-8 aspect-square overflow-hidden rounded-3xl">
+              <Image
+                src={image.data.url}
+                alt="Audiobook cover image"
+                fill={true}
+                placeholder="blur"
+                priority={true}
+                // TODO: See if this function runs a lot and memoize
+                blurDataURL={getBlurhashUrl(image.data.blurhash)}
+              />
+            </div>
+          </Tilt>
+        </>
+      )}
 
       <span>Similar Images</span>
       {similar.status === "pending" && <Spinner />}
