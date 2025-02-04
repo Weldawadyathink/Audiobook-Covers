9fc87b003ac2c3e7008eb3642064958349fab2de
Spenser Bushey <spenser@spenserbushey.com>
2025-02-04 08:40:18 +0100

    Add deno vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import deno from "@deno/vite-plugin"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), deno()],
   server: {
     proxy: {
       "/trpc": "http://localhost:8000"
