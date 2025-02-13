c6895a1a5ec8784f795e009713873347651b9f6f
Spenser Bushey <spenser@spenserbushey.com>
2025-02-13 16:29:53 +0100

    Update clip to support multiple models

--- src/server/utils/clip.ts
+++ src/server/utils/clip.ts
@@ -8,64 +8,142 @@ import {
 
 export const dimensions: number = 512;
 
-const model_id = "Benny1923/metaclip-b16-fullcc2.5b";
+export const models = {
+  "Benny1923/metaclip-b16-fullcc2.5b": { dimensions: 512 },
+  "Xenova/clip-vit-large-patch14": { dimensions: 768 },
+  "Xenova/mobileclip_blt": { dimensions: 512 },
+} as const;
 
-const tokenizer = await AutoTokenizer.from_pretrained(model_id);
-console.log("loaded tokenizer into memory");
-const text_model = await CLIPTextModelWithProjection.from_pretrained(
-  model_id,
-  { dtype: "fp32" },
-);
-console.log("loaded text_model into memory");
+export type ModelOptions = keyof typeof models;
 
-const processor = await AutoProcessor.from_pretrained(model_id, {});
-console.log("Loaded processor into memory");
-const vision_model = await CLIPVisionModelWithProjection.from_pretrained(
-  model_id,
-  { dtype: "fp32" },
-);
-console.log("loaded vision_model into memory");
+const global = globalThis as unknown as {
+  models: {
+    [K in ModelOptions]?: {
+      tokenizer: undefined | AutoTokenizer;
+      textModel: undefined | CLIPTextModelWithProjection;
+      processor: undefined | AutoProcessor;
+      visionModel: undefined | CLIPVisionModelWithProjection;
+    };
+  };
+};
 
-export async function getImageEmbedding(imageLocation: string) {
-  const image = await RawImage.read(imageLocation);
-  const imageInputs = await processor(image);
-  const { image_embeds } = await vision_model(imageInputs);
-  return [...image_embeds.normalize().data];
+function getGlobalModels(modelName: ModelOptions) {
+  if (!global.models) {
+    global.models = {};
+  }
+  if (!global.models[modelName]) {
+    global.models[modelName] = {
+      tokenizer: undefined,
+      textModel: undefined,
+      processor: undefined,
+      visionModel: undefined,
+    };
+  }
+  return global.models[modelName];
 }
 
-export async function getTextEmbedding(input: string) {
+async function getTextModel(modelName: ModelOptions): Promise<{
+  tokenizer: AutoTokenizer;
+  textModel: CLIPTextModelWithProjection;
+}> {
+  let { tokenizer, textModel } = getGlobalModels(modelName);
+  if (!tokenizer) {
+    tokenizer = await AutoTokenizer.from_pretrained(modelName);
+    global.models[modelName]!.tokenizer = tokenizer;
+    console.log(`Loaded tokenizer for ${modelName} into memory`);
+  }
+  if (!textModel) {
+    textModel = await CLIPTextModelWithProjection.from_pretrained(modelName, {
+      dtype: "fp32",
+    });
+    global.models[modelName]!.textModel = textModel;
+    console.log(`Loaded text model for ${modelName} into memory`);
+  }
+  return {
+    tokenizer,
+    textModel,
+  };
+}
+
+async function getVisionModel(modelName: ModelOptions): Promise<{
+  processor: AutoProcessor;
+  visionModel: CLIPVisionModelWithProjection;
+}> {
+  let { processor, visionModel } = getGlobalModels(modelName);
+  if (!processor) {
+    processor = await AutoProcessor.from_pretrained(modelName, {});
+    global.models[modelName]!.processor = processor;
+    console.log(`Loaded image processor for ${modelName} into memory`);
+  }
+  if (!visionModel) {
+    visionModel = await CLIPVisionModelWithProjection.from_pretrained(
+      modelName,
+      { dtype: "fp32" },
+    );
+    global.models[modelName]!.visionModel = visionModel;
+    console.log(`Loaded vision model for ${modelName} into memory`);
+  }
+  return {
+    processor,
+    visionModel,
+  };
+}
+
+export async function getImageEmbedding(
+  imageLocation: string,
+  modelName: ModelOptions,
+) {
+  const image = await RawImage.read(imageLocation);
+  const { processor, visionModel } = await getVisionModel(modelName);
+  const imageInputs = await processor(image);
+  const { image_embeds } = await visionModel(imageInputs);
+  return [...image_embeds.normalize().data] as Array<number>;
+}
+
+export async function getTextEmbedding(
+  input: string,
+  modelName: ModelOptions,
+) {
+  const { tokenizer, textModel } = await getTextModel(modelName);
   const tokens = tokenizer(input, { padding: "max_length", truncation: true });
-  const { text_embeds } = await text_model(tokens);
-  return [...text_embeds.normalize().data];
+  const { text_embeds } = await textModel(tokens);
+  return [...text_embeds.normalize().data] as Array<number>;
 }
 
 async function tests() {
-  let result: any = null;
-  console.log("Generating embedding for local image file");
-  result = await getImageEmbedding("./test_image.png");
-  console.log(
-    `Embedding created with ${result.length} dimensions. First value: ${
-      result[0]
-    }`,
-  );
+  for (const modelName of Object.keys(models)) {
+    let result: any = null;
 
-  console.log("Generating embedding for remote image file");
-  result = await getImageEmbedding(
-    "https://huggingface.co/datasets/Xenova/transformers.js-docs/resolve/main/football-match.jpg",
-  );
-  console.log(
-    `Embedding created with ${result.length} dimensions. First value: ${
-      result[0]
-    }`,
-  );
+    result = await getImageEmbedding(
+      "./test_image.png",
+      modelName as ModelOptions,
+    );
+    console.log(
+      `Embedding created with ${modelName}. ${result.length} dimensions. First value: ${
+        result[0]
+      }`,
+    );
 
-  console.log("Generating embedding for string");
-  result = await getTextEmbedding("Hello World!");
-  console.log(
-    `Embedding created with ${result.length} dimensions. First value: ${
-      result[0]
-    }`,
-  );
+    result = await getImageEmbedding(
+      "https://huggingface.co/datasets/Xenova/transformers.js-docs/resolve/main/football-match.jpg",
+      modelName as ModelOptions,
+    );
+    console.log(
+      `Embedding created with ${modelName}. ${result.length} dimensions. First value: ${
+        result[0]
+      }`,
+    );
+
+    result = await getTextEmbedding(
+      "Hello World!",
+      modelName as ModelOptions,
+    );
+    console.log(
+      `Embedding created with ${modelName}. ${result.length} dimensions. First value: ${
+        result[0]
+      }`,
+    );
+  }
 }
 
-// await tests();
+await tests();
