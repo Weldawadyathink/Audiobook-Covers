6b26c5d7a1e0fbe94dd89022bccd6eb417067fdb
Spenser Bushey <spenser@spenserbushey.com>
2025-02-15 17:42:46 +0100

    Added search page form

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -9,8 +9,14 @@ import { BlurhashCanvas } from "react-blurhash";
 import { Link } from "@tanstack/react-router";
 import { useBlurhashUrl } from "../utils/blurhash.ts";
 
-export function ImageCard(props: { imageData: ImageData; className?: string }) {
-  const maxAngle = 15;
+export function ImageCard(
+  props: {
+    imageData: ImageData;
+    className?: string;
+    hideSourceBadge?: boolean;
+  },
+) {
+  const maxAngle = 7;
   const [isHovered, setIsHovered] = useState(false);
   const [isLoaded, setIsLoaded] = useState(false);
 
@@ -28,7 +34,7 @@ export function ImageCard(props: { imageData: ImageData; className?: string }) {
 
   return (
     <Tilt
-      scale={1.3}
+      scale={1.15}
       transitionSpeed={900}
       tiltMaxAngleX={maxAngle}
       tiltMaxAngleY={maxAngle}
@@ -38,17 +44,20 @@ export function ImageCard(props: { imageData: ImageData; className?: string }) {
       // TODO: Find out why transform-style-3d breaks clicking on Link elements
       className={cn(
         props.className,
-        "relative aspect-square cursor-pointer, overflow-hidden rounded-3xl",
+        "relative aspect-square cursor-pointer, overflow-hidden rounded-3xl transform-3d",
         isHovered ? "z-10" : "z-0",
       )}
       style={style}
     >
-      <Badge className="absolute z-20 aspect-square transform perspective-200 translate-z-5">
-        <IconBrandReddit />
-      </Badge>
+      {!props.hideSourceBadge && (
+        <Badge className="top-1 left-1 bg-stone-700 opacity-80 rounded-full absolute z-20 aspect-square transform perspective-200 translate-z-5">
+          <IconBrandReddit />
+        </Badge>
+      )}
       <Link to={`/image/${props.imageData.id}`}>
         {!isLoaded && (
           <BlurhashCanvas
+            className="h-full w-full"
             hash={props.imageData.blurhash}
           />
         )}
--- src/routes/image/search.tsx
+++ src/routes/image/search.tsx
@@ -3,24 +3,80 @@ import { Input } from "../../components/ui/Input.tsx";
 import { Button } from "../../components/ui/Button.tsx";
 import { api } from "../../utils/trpc.tsx";
 import { ImageCard } from "../../components/ImageCard.tsx";
+import { Spinner } from "../../components/Spinner.tsx";
+import { z } from "zod";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+} from "../../components/ui/Form.tsx";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export const Route = createFileRoute("/image/search")({
   component: RouteComponent,
 });
 
+const formSchema = z.object({
+  q: z.string().trim().min(1),
+});
+
 function RouteComponent() {
-  const covers = api.cover.vectorSearchWithString.useQuery({
-    modelName: "Benny1923/metaclip-b16-fullcc2.5b",
-    queryString: "Hello world!",
+  const [query, setQuery] = useState("");
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      q: "",
+    },
   });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    setQuery(values.q);
+  }
+
+  const images = api.cover.vectorSearchWithString.useQuery({
+    modelName: "Benny1923/metaclip-b16-fullcc2.5b",
+    queryString: query,
+  }, {
+    enabled: () => query !== "",
+  });
+
   return (
-    <div>
-      <Input></Input>
-      <Button>Search</Button>
-      {covers.isSuccess &&
-        covers.data.map((item) => (
-          <ImageCard imageData={item} className="max-w-48 max-h-48" />
-        ))}
+    <div className="mx-6">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-row justify-center gap-6"
+        >
+          <FormField
+            control={form.control}
+            name="q"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    className="w-auto max-w-80 text-black"
+                    placeholder="Enter a search term..."
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+      {images.isLoading && <Spinner />}
+      {images.isSuccess && (
+        <div className="flex flex-wrap justify-center gap-6 p-12">
+          {images.data.length === 0 && <p>Could not find any results</p>}
+          {images.data.map((image) => (
+            <ImageCard key={image.id} imageData={image} className="w-56" />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
