c933a099a5827cc44e7f95e5ddf75c2c0880d328
Spenser Bushey <spenser@spenserbushey.com>
2025-05-24 16:41:48 +0200

    Revert "Server side trpc working"
    This reverts commit 7328819c14afe1558f479cab65a7e10862ae387d.

--- server/fetch-context.ts
+++
@@ -1,9 +0,0 @@
-import { FetchCreateContextFnOptions } from "@trpc/server/adapters/fetch";
-
-export function createContext({ req }: FetchCreateContextFnOptions) {
-  //   const user = { name: req.headers.get("username") ?? "anonymous" };
-  //   return { req, user };
-  return { req };
-}
-
-export type Context = Awaited<ReturnType<typeof createContext>>;
