fea28220e8b3aacc9d301874d8d8c44ed8f29a2b
Spenser Bushey <spenser@spenserbushey.com>
2025-06-09 12:23:21 +0200

    Remove hono

--- downloader/main.ts
+++ downloader/main.ts
@@ -1,23 +1,3 @@
-import { Hono } from "hono";
-
-const validAPIKey = Deno.env.get("API_KEY");
-if (!validAPIKey) {
-  console.log("API_KEY environment variable is missing");
-  Deno.exit(1);
-}
-const validAPIAuth = `Bearer ${validAPIKey}`;
-
-const app = new Hono();
-
-app.post("/download", (c) => {
-  const auth = c.req.header("Authorization");
-  if (!auth || auth !== validAPIAuth) {
-    console.log("Unauthorized access");
-    c.status(401);
-    return c.text("Not authorized");
-  }
-  console.log("Authorized access");
-  return c.text("Hello Hono!");
-});
-
-Deno.serve(app.fetch);
+console.log("Deno is running");
+console.log("Pretend to run downloader");
+setTimeout(() => console.log("Downloader complete"), 5000);
