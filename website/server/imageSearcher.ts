04891ccf4970d768a09c571a446028617befbd00
Spenser Bushey <spenser@spenserbushey.com>
2025-06-10 21:17:40 +0200

    Fix select all in general database lookups

--- website/server/imageSearcher.ts
+++ website/server/imageSearcher.ts
@@ -4,28 +4,42 @@ import { defaultModel, ModelOptions, models } from "./models.ts";
 
 export async function getRandom() {
   console.log("Getting random cover");
+  const start = performance.now();
   const results = await pool.many(
     sql.typeAlias("imageData")`
-      SELECT *
+      SELECT
+        id,
+        source,
+        extension,
+        blurhash
       FROM image
       WHERE searchable
       ORDER BY RANDOM()
       LIMIT 54
     `,
   );
+  const time = performance.now() - start;
+  console.log(`getRandom database lookup in ${time.toFixed(1)}ms`);
   return await shapeImageData(results);
 }
 
 export async function getImageById(id: string) {
   console.log(`getById: ${id}`);
+  const start = performance.now();
   const results = await pool.maybeOne(
     sql.typeAlias("imageData")`
-      SELECT *
+      SELECT
+        id,
+        source,
+        extension,
+        blurhash
       FROM image
       WHERE id=${id}
       LIMIT 1
     `,
   );
+  const time = performance.now() - start;
+  console.log(`getImageById database lookup in ${time.toFixed(1)}ms`);
   if (!results) {
     return;
   }
