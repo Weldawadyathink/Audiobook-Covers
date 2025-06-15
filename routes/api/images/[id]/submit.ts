661ffa22c7c195bfc60e417befb165441b96e9bc
Spenser Bushey <spenser@spenserbushey.com>
2025-06-15 09:53:32 +0200

    Allow admin to edit image searchablility

--- routes/api/images/[id]/submit.ts
+++ routes/api/images/[id]/submit.ts
@@ -1,5 +1,11 @@
 import { define } from "../../../../utils.ts";
 import { getIsAuthenticated } from "../../../../server/auth.ts";
+import { z } from "zod/v4";
+import { getDbPool, sql } from "../../../../server/db.ts";
+
+const formValidator = z.object({
+  searchable: z.stringbool(),
+});
 
 export const handler = define.handlers({
   async POST(ctx) {
@@ -9,7 +15,24 @@ export const handler = define.handlers({
     }
     const formData = await ctx.req.formData();
     console.log(formData);
-    console.log(ctx.params.id);
+    const form = formValidator.safeParse(
+      Object.fromEntries(formData.entries()),
+    );
+    console.log(form);
+    if (!form.success) {
+      return new Response("Could not process form data", { status: 400 });
+    }
+
+    // Request is valid, update database
+    const pool = await getDbPool();
+    await pool.query(
+      sql.typeAlias("void")`
+        UPDATE image
+        SET searchable = ${form.data.searchable}
+        WHERE id = ${ctx.params.id}
+      `,
+    );
+
     const headers = new Headers();
     headers.set("Location", `/images/${ctx.params.id}`);
     return new Response("Submitted", {
