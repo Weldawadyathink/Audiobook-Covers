1a7f9aafd222f711f6f35f1067551e6e3becd305
Spenser Bushey <spenser@spenserbushey.com>
2025-06-15 13:51:52 +0200

    Finally fix health checks

--- routes/api/healthcheck.ts
+++ routes/api/healthcheck.ts
@@ -1,7 +1,9 @@
 import { define } from "../../utils.ts";
+import { getDbPool } from "../../server/db.ts";
 
 export const handler = define.handlers({
   GET(ctx) {
+    const _ = getDbPool(); // Ensure database connection exists, but don't await
     return new Response(
       `Server is healthy!`,
     );
