4b5c551a0f6cb7831e7b1c6691978bd3a1cdee1b
Spenser Bushey <spenser@spenserbushey.com>
2025-06-17 08:01:43 +0200

    Add env validation

--- src/env.ts
+++ src/env.ts
@@ -1,28 +1,7 @@
 import { z } from "zod/v4";
-import { parseEnv } from "@Weldawadyathink/zod-env"; //TODO: Switch to @keawade/zod-env once PR is merged
 
-const envValidator = z.object({
-  DATABASE_URL: z.url(),
-  REPLICATE_API_TOKEN: z.string(),
-  NODE_ENV: z.enum(["development", "build", "production"]),
-});
-
-type Env = z.infer<typeof envValidator>;
-
-const builder_env = parseEnv(z.object({
-  NODE_ENV: z.enum(["development", "build", "production"]),
-}));
-
-let env: Env;
-
-if (builder_env.NODE_ENV === "build") {
-  console.log(
-    "Running in build mode, environment variables are not type checked",
-  );
-  // env variables not populated, but should not be needed during build
-  env = builder_env as Env;
-} else {
-  env = parseEnv(envValidator);
-}
-
-export { env };
+export const env = z
+  .object({
+    DATABASE_URL: z.url(),
+  })
+  .parse(process.env);
