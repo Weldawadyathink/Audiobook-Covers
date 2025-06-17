7839b6de37363cfb5833e72eda78b5d9755cbdf0
Spenser Bushey <spenser@spenserbushey.com>
2025-06-17 22:05:54 +0200

    Add search form

--- src/routes/search.tsx
+++ src/routes/search.tsx
@@ -1,8 +1,11 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { createFileRoute, Outlet, useNavigate } from "@tanstack/react-router";
 import { z } from "zod/v4";
 import { zodValidator } from "@tanstack/zod-adapter";
 import { vectorSearchByString } from "@/server/imageSearcher";
 import ImageCard from "@/components/ImageCard";
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 const searchParameters = z.object({
   q: z.string().default(""),
@@ -15,6 +18,8 @@ export const Route = createFileRoute("/search")({
   loaderDeps: ({ search: { q, model } }) => ({ q, model }),
   loader: async ({ deps: data }) => {
     return {
+      q: data.q,
+      model: data.model,
       images: await vectorSearchByString({ data }),
       auth: true,
     };
@@ -22,10 +27,34 @@ export const Route = createFileRoute("/search")({
 });
 
 function RouteComponent() {
-  const { images, auth } = Route.useLoaderData();
+  const { images, auth, q, model } = Route.useLoaderData();
+  const [searchQuery, setSearchQuery] = useState(q);
+  const navigate = useNavigate();
+  const submitForm = () => {
+    navigate({
+      to: "/search",
+      search: {
+        q: searchQuery,
+        model: model,
+      },
+    });
+  };
   return (
     <>
-      <div>Hello "/search/"!</div>
+      <form
+        className="flex gap-6 mx-36"
+        onSubmit={(e) => {
+          e.preventDefault();
+          submitForm();
+        }}
+      >
+        <Input
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <Button type="submit">Search</Button>
+      </form>
       <div className="grid md:grid-cols-4 justify-center gap-6 sm:grid-cols-2 mx-6 my-6">
         {images.map((image) => (
           <ImageCard
