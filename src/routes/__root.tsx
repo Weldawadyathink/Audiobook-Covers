ecd8e1f263b2742c6807c9499b090b39544f5f13
Spenser Bushey <spenser@spenserbushey.com>
2025-06-17 07:57:02 +0200

    Add prettier and reformat project

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,71 +1,71 @@
-import type { ReactNode } from 'react'
+import type { ReactNode } from "react";
 import {
   Outlet,
   createRootRoute,
   HeadContent,
   Scripts,
-} from '@tanstack/react-router'
+} from "@tanstack/react-router";
 // @ts-ignore For some reason it doesn't like this pattern
-import appCss from '@/styles/app.css?url'
+import appCss from "@/styles/app.css?url";
 
 export const Route = createRootRoute({
   head: () => ({
     meta: [
       {
-        charSet: 'utf-8',
+        charSet: "utf-8",
       },
       {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
       },
       {
-        title: 'TanStack Start Starter',
+        title: "TanStack Start Starter",
       },
     ],
     links: [
-      { rel: 'stylesheet', href: appCss },
+      { rel: "stylesheet", href: appCss },
       {
-        rel: 'apple-touch-icon',
-        sizes: '180x180',
-        href: '/apple-touch-icon.png',
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
       },
       {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '32x32',
-        href: '/favicon-32x32.png',
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png",
       },
       {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '16x16',
-        href: '/favicon-16x16.png',
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png",
       },
-      { rel: 'manifest', href: '/site.webmanifest', color: '#fffff' },
-      { rel: 'icon', href: '/favicon.ico' },
-    ]
+      { rel: "manifest", href: "/site.webmanifest", color: "#fffff" },
+      { rel: "icon", href: "/favicon.ico" },
+    ],
   }),
   component: RootComponent,
-})
+});
 
 function RootComponent() {
   return (
     <RootDocument>
       <Outlet />
     </RootDocument>
-  )
+  );
 }
 
 function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
   return (
     <html>
-    <head>
-      <HeadContent />
-    </head>
-    <body>
-    {children}
-    <Scripts />
-    </body>
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+        <Scripts />
+      </body>
     </html>
-  )
+  );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,48 +1,48 @@
-import * as fs from 'node:fs'
-import { createFileRoute, useRouter } from '@tanstack/react-router'
-import { createServerFn } from '@tanstack/react-start'
-import {Button} from "@/components/ui/button";
+import * as fs from "node:fs";
+import { createFileRoute, useRouter } from "@tanstack/react-router";
+import { createServerFn } from "@tanstack/react-start";
+import { Button } from "@/components/ui/button";
 
-const filePath = 'count.txt'
+const filePath = "count.txt";
 
 async function readCount() {
   return parseInt(
-    await fs.promises.readFile(filePath, 'utf-8').catch(() => '0'),
-  )
+    await fs.promises.readFile(filePath, "utf-8").catch(() => "0"),
+  );
 }
 
 const getCount = createServerFn({
-  method: 'GET',
+  method: "GET",
 }).handler(() => {
-  return readCount()
-})
+  return readCount();
+});
 
-const updateCount = createServerFn({ method: 'POST' })
+const updateCount = createServerFn({ method: "POST" })
   .validator((d: number) => d)
   .handler(async ({ data }) => {
-    const count = await readCount()
-    await fs.promises.writeFile(filePath, `${count + data}`)
-  })
+    const count = await readCount();
+    await fs.promises.writeFile(filePath, `${count + data}`);
+  });
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: Home,
   loader: async () => await getCount(),
-})
+});
 
 function Home() {
-  const router = useRouter()
-  const state = Route.useLoaderData()
+  const router = useRouter();
+  const state = Route.useLoaderData();
 
   return (
     <Button
       type="button"
       onClick={() => {
         updateCount({ data: 1 }).then(() => {
-          router.invalidate()
-        })
+          router.invalidate();
+        });
       }}
     >
       Add 1 to {state}?
     </Button>
-  )
+  );
 }
