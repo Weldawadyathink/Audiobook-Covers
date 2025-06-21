490469ec61516c40efed0b6d3a187b9c8c0432d7
Spenser Bushey <spenser@spenserbushey.com>
2025-06-21 21:57:31 +0200

    Change title

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -31,7 +31,7 @@ export const Route = createRootRoute({
         content: "width=device-width, initial-scale=1",
       },
       {
-        title: "TanStack Start Starter",
+        title: "AudiobookCovers.com",
       },
     ],
     links: [
