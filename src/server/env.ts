359dc494a7c74b0493297f5193f58d1ea931b252
Spenser Bushey <spenser@spenserbushey.com>
2026-02-10 21:45:06 -0800

    Begin google embedding support

--- src/server/env.ts
+++ src/server/env.ts
@@ -6,6 +6,9 @@ const serverEnvSchema = z.object({
   DATABASE_URL: z.url(),
   REPLICATE_API_TOKEN: z.string(),
   APP_STAGE: z.enum(["local", "development", "production"]),
+  GOOGLE_CLOUD_PROJECT: z.string(),
+  GOOGLE_VERTEX_LOCATION: z.string(),
+  GOOGLE_API_KEY: z.string(),
 });
 
 export const getEnv = createIsomorphicFn()
