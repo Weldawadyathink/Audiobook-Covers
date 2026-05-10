d0ca6b19d6f1dc79da6663481de76bdc6c418de4
Spenser Bushey <spenser@spenserbushey.com>
2026-05-09 22:28:01 -0700

    Update packages

--- src/env.cloudflare.ts
+++ src/env.cloudflare.ts
@@ -3,7 +3,11 @@ import { env as workerEnv } from "cloudflare:workers";
 import "@tanstack/react-start/server-only";
 import "dotenv/config";
 
+console.log(workerEnv);
+console.log(process.env);
+
 export const env = parseEnv({
+  ...process.env,
   ...workerEnv,
   DATABASE_READ_URL: workerEnv.HYPERDRIVE?.connectionString!,
   DATABASE_WRITE_URL: workerEnv.HYPERDRIVE?.connectionString!,
