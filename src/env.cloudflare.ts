21f462cebc420902bdf6a0d9c343b464d0f27719
Spenser Bushey <spenser@spenserbushey.com>
2026-05-09 22:38:26 -0700

    Fixed environments

--- src/env.cloudflare.ts
+++ src/env.cloudflare.ts
@@ -3,12 +3,13 @@ import { env as workerEnv } from "cloudflare:workers";
 import "@tanstack/react-start/server-only";
 import "dotenv/config";
 
-console.log(workerEnv);
-console.log(process.env);
-
 export const env = parseEnv({
   ...process.env,
   ...workerEnv,
-  DATABASE_READ_URL: workerEnv.HYPERDRIVE?.connectionString!,
-  DATABASE_WRITE_URL: workerEnv.HYPERDRIVE?.connectionString!,
+
+  // Cloudflare is too lazy to inject hyperdrive connection strings during dev
+  DATABASE_READ_URL:
+    workerEnv.HYPERDRIVE?.connectionString || process.env.DATABASE_READ_URL,
+  DATABASE_WRITE_URL:
+    workerEnv.HYPERDRIVE?.connectionString || process.env.DATABASE_WRITE_URL,
 });
